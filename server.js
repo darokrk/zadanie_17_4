a03342646455d6d8ed21a867de0d2a52b2c87053
Dariusz Rosiek <daro.krk@interia.pl>
2018-04-02 21:49:41 +0200

    Create express test app to upload files

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('assets'));
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+var server = app.listen(4000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Przykładowa aplikacja nasłuchuje na na http://' + host + ':' + port);
+});
