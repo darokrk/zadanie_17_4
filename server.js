dfe25dde7316827dcb4f181e490b8b836abd57d8
Dariusz Rosiek <daro.krk@interia.pl>
2018-04-02 22:07:37 +0200

    Upgrade express app code to save form submit inputs

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var app = express();
-
 app.use(express.static('assets'));
+
 app.get('/', function (req, res) {
-    res.send('Hello World');
+    res.sendFile('/index.html');
+});
+
+app.get('/userform', function (req, res) {
+    const response = {
+        first_name: req.query.first_name,
+        last_name: req.query.last_name
+    };
+    res.end(JSON.stringify(response));
 });
 
 var server = app.listen(4000, 'localhost', function() {
